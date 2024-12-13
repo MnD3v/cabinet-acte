193a679aa3c0e743c50c5d88ad8aca324ec11f58
djatika dineman emanuel <lolo36626835@gmail.com>
2024-12-13 03:40:04 +0000

    la

--- src/app/components/equipe.tsx
+++ src/app/components/equipe.tsx
@@ -7,8 +7,6 @@ const Equipe = () => {
     { image: "/equipe/vice-pdg.png", poste: "Consultant formateur, informaticien", nom: "Justin HOULOUMIE", note: "4.8" }, { image: "/equipe/aubin.png", poste: "Consultant formateur, Graphiste designer", nom: "Aubin KUEVIAKOE", note: "4.7" },
     { image: "/equipe/manu.png", poste: "Consultant formateur, Développeur ", nom: "Emanuel DJATIKA", note: "4.7" },
     { image: "/equipe/vice-pdg.png", poste: "Drt, Gestionnaire Comptable agrée", nom: "R. Kangni KUEVIAKOE", note: "4.7" },
-
-
     ]
     return (
         <div className='flex justify-center'>
